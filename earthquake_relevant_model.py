be609c078356367b0682ec3a4fd403a1a805dd14
Sherly <emirhandelen13@gmail.com>
2025-03-19 16:15:33 +0300

    silly bug

--- earthquake_relevant_model.py
+++ earthquake_relevant_model.py
@@ -1,12 +1,12 @@
-import torch
-from models import BinaryClassifier, process_batch_binary_classification, get_model
+from models import process_batch_binary_classification, get_binary_model
 from utils import preprocess
 from my_token import token
 
 if __name__ == "__main__":
     texts = ["7.1 büyüklüğündeki felakat bir çok can aldı.", "Bu da bir test cümlesidir"]
+    texts = [preprocess(text) for text in texts]
     model_path = "Politus/earthquake-binary-dbmdz-bert-base-turkish-128k-uncased-128"
     tokenizer_path = "dbmdz/bert-base-turkish-128k-uncased"
-    model, tokenizer, device = get_model(model_path, tokenizer_path, token)
+    model, tokenizer, device = get_binary_model(model_path, tokenizer_path, token)
     predictions = process_batch_binary_classification(texts, model, tokenizer, device)
     print(predictions)
