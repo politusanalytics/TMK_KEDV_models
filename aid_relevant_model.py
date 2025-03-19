483b6f69c637bf6089d4bfe92e2b36d393e75fe3
Sherly <emirhandelen13@gmail.com>
2025-03-19 14:17:10 +0300

    adding token

--- aid_relevant_model.py
+++ aid_relevant_model.py
@@ -1,12 +1,12 @@
 import torch
 from models import BinaryClassifier, process_batch_binary_classification, get_model
 from utils import preprocess
-
+from my_token import token
 
 if __name__ == "__main__":
     texts = ["Bu bir test cümlesidir", "Bu da bir test cümlesidir"]
     model_path = "Politus/oxfam_help-binary-dbmdz-bert-base-turkish-128k-uncased-128"
     tokenizer_path = "dbmdz/bert-base-turkish-128k-uncased"
-    model, tokenizer, device = get_model(model_path, tokenizer_path)
+    model, tokenizer, device = get_model(model_path, tokenizer_path, token)
     predictions = process_batch_binary_classification(texts, model, tokenizer, device)
     print(predictions)
